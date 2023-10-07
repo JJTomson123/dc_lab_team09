694537ddde0a610ecc4585dac25266227f51b0a9
MintChocoManju <mnln2468@gmail.com>
2023-10-07 16:43:49 +0800

    Added core testbench run file

---
+++ lab2/src/tb_verilog/tb_core_run.f
@@ -0,0 +1,8 @@
+-sverilog 
+tb.sv 
+../Rsa256Core.sv
++access+rw
+-P /usr/cad/synopsys/verdi/cur/share/PLI/VCS/LINUX64/novas.tab
+/usr/cad/synopsys/verdi/cur/share/PLI/VCS/LINUX64/pli.a
+
+# vcs -full64 -R -f run.f
