77abe30f2ef4005abd2a9edf3950eb73b1b7f374
JJTomson123 <changvv2018@gmail.com>
2023-10-26 14:22:23 +0800

    tb fin, but i2c undone

--- lab3/src/I2cInitializer.sv
+++ lab3/src/I2cInitializer.sv
@@ -25,46 +25,91 @@ localparam ActiveControl = 24'b0011_0100_000_1001_0_0000_0001;
 logic [1:0] state_r, state_w;
 logic sdat_r, sdat_w, sclk_r, sclk_w;
 logic [167:0] setting_data_r, setting_data_w;
+logic [3:0] bit_counter_r, bit_counter_w;
+logic enable_r, enable_w, done_r, done_w;
 
-logic [2:0] bit_counter_r, bit_counter_w;
+assign o_sclk = sclk_r;
+assign o_sdat = sdat_r;
+assign o_oen = enable_r;
+assign o_finished = done_r;
 
 always_comb begin
     setting_data_w = setting_data_r;
+    done_w = done_r;
 	case(state_r)
     S_IDLE: begin
+        sclk_w = 1;
+        bit_counter_w = 0;
         if (i_start) begin
             sdat_w = 0;
             state_w = S_CHANGE;
+            enable_w = 1;
         end else begin
             sdat_w = 1;
             state_w = S_IDLE;
+            enable_w = 0;
         end
     end
     S_CHANGE: begin
         sclk_w = 0;
         if (sclk_r) begin
-            sdat_w = sdat_r;
             state_w = S_CHANGE;
-        end else begin
-            sdat_w = setting_data_r[167];
-            setting_data_w = setting_data_r << 1;
+            sdat_w = sdat_r;
+            bit_counter_w = bit_counter_r;
+            enable_w = enable_r;
+        end 
+        else begin
             state_w = S_READ;
+            if (bit_counter_r == 4'b1000) begin
+                sdat_w = 1'bz;
+                bit_counter_w = 0;
+                enable_w = 0;
+            end
+            else begin
+                sdat_w = sdat_r;
+                sdat_w = setting_data_r[167];
+                setting_data_w = setting_data_r << 1;
+                bit_counter_w = bit_counter_r + 1;
+                enable_w = enable_r;
+            end
         end
     end
     S_READ: begin
         sclk_w = 1;
+        bit_counter_w = bit_counter_r;
+        sdat_w = sdat_r;
         if (sclk_r) begin
-            state_r
-
+            if (setting_data_r==0) begin
+                state_w = S_END;
+                enable_w = 0;
+            end else begin
+                state_w = S_CHANGE;
+                enable_w = 1;
+            end
+        end
+        else begin
+            state_w = S_READ;
+            enable_w = enable_r;
         end
     end
     S_END: begin
-
+        sdat_w = 1;
+        sclk_w = 1;
+        enable_w = enable_r;
+        bit_counter_w = 0;
+        if (sdat_r) begin
+            state_w = S_IDLE;
+            done_w = 1;
+        end
+        else begin
+            state_w = S_END;
+            done_w = 0;
+        end
     end
     endcase
 end
 
-always_ff @(posedge i_AUD_BCLK or posedge i_rst_n) begin
+always_ff @(posedge i_clk or posedge i_rst_n) begin
 	if (!i_rst_n) begin
         state_r <= S_IDLE;
         sclk_r <= 1;
@@ -76,12 +121,20 @@ always_ff @(posedge i_AUD_BCLK or posedge i_rst_n) begin
                         DigitalAudioInterfaceFormat,
                         SamplingControl,
                         ActiveControl};
+        done_r <= 0;
+        enable_r <= 0;
+        bit_counter_r <= 0;
 
 		
 	end
 	else begin
         state_r <= state_w;
         setting_data_r <= setting_data_w;
+        sclk_r <= sclk_w;
+        sdat_r <= sdat_w;
+        done_r <= done_w;
+        enable_r <= enable_w;
+        bit_counter_r <= bit_counter_w;
 		
 	end
 end
--- lab3/src/tb/i2c_tb.sv
+++ lab3/src/tb/i2c_tb.sv
@@ -2,18 +2,60 @@ module tb;
 	localparam CLK = 10;
 	localparam HCLK = CLK/2;
 
-	logic clk, start_cal, fin, rst;
-	initial clk = 0;
-	always #HCLK clk = ~clk;
+	logic i_clk_100K, start_cal, fin, i_rst_n;
+	initial i_clk_100K = 0;
+	always #HCLK i_clk_100K = ~i_clk_100K;
+	logic o_I2C_SCLK,i2c_oen, ack;
+	logic [167:0] data;
+	wire i2c_sdat;
+
 
     I2cInitializer init0(
 	.i_rst_n(i_rst_n),
 	.i_clk(i_clk_100K),
-	.i_start(),
-	.o_finished(),
+	.i_start(start_cal),
+	.o_finished(fin),
 	.o_sclk(o_I2C_SCLK),
 	.o_sdat(i2c_sdat),
 	.o_oen(i2c_oen) // you are outputing (you are not outputing only when you are "ack"ing.)
     );
+
+	assign i2c_sdat = (ack && !i2c_oen) ? 1'b0 : 1'bz;
+
+	initial begin
+		$fsdbDumpfile("lab3_i2c.fsdb");
+		$fsdbDumpvars;
+		i_rst_n = 0;
+		ack = 0;
+		#(2*CLK)
+		i_rst_n = 1;
+		@(posedge i_clk_100K)
+		start_cal <= 1;
+		@(posedge i_clk_100K)
+		start_cal <= 0;
+		for (int i = 0; i < 21; i++) begin
+			for (int j = 0; j < 8; j++) begin
+				@(posedge o_I2C_SCLK);
+				data = {data[166:0],i2c_sdat};
+			end
+			@(negedge o_I2C_SCLK);
+			ack = 1;
+			@(negedge o_I2C_SCLK);
+			ack = 0;
+		end
+		@(posedge fin)
+		$display("=========");
+		$display("i2c_data  %168b", data);
+		$display("=========");
+		$finish;
+	end
+
+	initial begin
+		#(500000*CLK)
+		$display("Too slow, abort.");
+		$finish;
+	end
+
+	
 
 endmodule
