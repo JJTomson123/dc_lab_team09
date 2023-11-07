2e110e72909a41bf03bbecb142b28feff59ab8d1
JJTomson123 <changvv2018@gmail.com>
2023-11-07 12:08:56 +0800

    Why still doesn't work?

--- lab3/src/SDRAMWrapper.sv
+++ lab3/src/SDRAMWrapper.sv
@@ -9,16 +9,16 @@ module SDRAMWrapper(
     output [15:0] o_data,
     output        o_valid,
 
-    input  [31:0] i_sdrmctrl_readdata,
-    input         i_sdrmctrl_readdatavalid,
-    input         i_sdrmctrl_waitrequest,
+    input  [31:0] avm_readdata,
+    input         avm_readdatavalid,
+    input         avm_waitrequest,
 
-	output [24:0] o_sdrmctrl_address,
-    output [3:0]  o_sdrmctrl_byteenable_n,
-    output        o_sdrmctrl_chipselect,
-    output [31:0] o_sdrmctrl_writedata,
-    output        o_sdrmctrl_read_n,
-    output        o_sdrmctrl_write_n
+	output [24:0] avm_address,
+    output [3:0]  avm_byteenable_n,
+    output        avm_chipselect,
+    output [31:0] avm_writedata,
+    output        avm_read_n,
+    output        avm_write_n
 );
 
 
@@ -29,40 +29,58 @@ localparam S_READ  = 2;
 logic [2:0]  state_r, state_w;
 logic [25:0] addr_r, addr_w;
 logic [15:0] data_r, data_w;
+logic        valid_r, valid_w;
+logic        read_r, read_w, write_r, write_w;
 
-assign o_data  = (addr_r[0]) ? i_sdrmctrl_readdata[31:16] : i_sdrmctrl_readdata[15:0];
-assign o_valid = i_sdrmctrl_readdatavalid;
+assign o_data  = data_r;
+assign o_valid = 1'b1;
 
-assign o_sdrmctrl_address      = addr_r[24:0];
-assign o_sdrmctrl_byteenable_n = (addr_r[0]) ? 4'b0011 : 4'b1100;
-assign o_sdrmctrl_chipselect   = 1'b1;
-assign o_sdrmctrl_writedata    = (addr_r[0]) ? {data_r, 16'b0} : {16'b0, data_r};
-assign o_sdrmctrl_read_n       = !(state_r == S_READ);
-assign o_sdrmctrl_write_n      = !(state_r == S_WRITE);
+assign avm_address      = addr_r[24:0];
+assign avm_byteenable_n = (addr_r[0]) ? 4'b0011 : 4'b1100;
+assign avm_chipselect   = 1'b1;
+assign avm_writedata    = (addr_r[0]) ? {data_r, 16'b0} : {16'b0, data_r};
+assign avm_read_n       = !read_r;
+assign avm_write_n      = !write_r;
 
 always_comb begin
     // FSM
     addr_w    = addr_r;
     data_w    = data_r;
+    valid_w   = valid_r;
+    read_w    = 0;
+    write_w   = 0;
     case(state_r)
     S_IDLE: begin
         if (i_write) begin
             state_w = S_WRITE;
             addr_w  = i_addr;
             data_w  = i_data;
+            write_w = 1;
         end else if (i_read) begin
             state_w = S_READ;
             addr_w  = i_addr;
+            valid_w = 0;
+            read_w  = 1;
         end
         else state_w = S_IDLE;
     end
     S_WRITE: begin
-        if (i_sdrmctrl_waitrequest) state_w = S_WRITE;
-        else                        state_w = S_IDLE;
+        if (avm_waitrequest) begin 
+            state_w = S_WRITE;
+            write_w = 1;
+        end
+        else                 state_w = S_IDLE;
     end
     S_READ: begin
-        if (i_sdrmctrl_waitrequest) state_w = S_READ;
-        else                        state_w = S_IDLE;
+        if (avm_waitrequest || !avm_readdatavalid) begin
+            state_w = S_READ;
+            read_w  = 1;
+        end
+        else begin
+            state_w = S_IDLE;
+            data_w  = (addr_r[0]) ? avm_readdata[31:16] : avm_readdata[15:0];
+            valid_w = 1;
+        end
     end
     endcase
 end
@@ -72,11 +90,17 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
         state_r   <= S_IDLE;
         data_r    <= 0;
         addr_r    <= 0;
+        valid_r   <= 0;
+        read_r    <= 0;
+        write_r   <= 0;
 	end
 	else begin
         state_r   <= state_w;
         data_r    <= data_w;
         addr_r    <= addr_w;
+        valid_r   <= valid_w;
+        read_r    <= read_w;
+        write_r   <= write_w;
 	end
 end
 
