d83bbb8fa41625b909c9cb5db8360664356336ff
JJTomson123 <changvv2018@gmail.com>
2023-12-28 17:23:56 +0800

    Backup

--- final_project/src/Top.sv
+++ final_project/src/Top.sv
@@ -35,6 +35,10 @@ localparam S_PREP2 = 9;
 localparam S_PREP3 = 10;
 localparam S_PREP4 = 11;
 localparam S_PREP5 = 12;
+localparam S_STSZ1 = 13;
+localparam S_STSZ2 = 14;
+localparam S_GTSZ  = 15;
+localparam S_SDSZ  = 16;
 
 localparam ADD   = 4'b0000;
 localparam SUB   = 4'b0001;
@@ -55,24 +59,28 @@ logic [19:0] var_addr_r, var_addr_w;
 logic [19:0] SRAM_addr, add_addr, mul_addr;
 logic [19:0] top_addr_r, top_addr_w;
 logic [15:0] top_wdata_r, top_wdata_w;
+logic        top_wen_r, top_wen_w;
 logic        add_valid_w, mul_valid_w, add_sub, mul_valid_r, add_valid_r;
 logic [16:0] varsize_x1_r, varsize_x1_w, varsize_x2_r, varsize_x2_w, varsize_r, varsize_w;
+logic [23:0] varsize_x3_r, varsize_x3_w;
 logic [19:0] x1_addr_r, x1_addr_w, x2_addr_r, x2_addr_w, x3_addr_r, x3_addr_w;
 logic        SRAM_wen, add_wen, mul_wen, add_done, mul_done;
 logic [15:0] SRAM_wdata, add_wdata, mul_wdata, SRAM_rdata;
+logic [16:0] add_size3, mul_size3;
 logic [4:0] rs_addr_r, rs_addr_w;
 logic [3:0] inst_r, inst_w, x1_r, x1_w, x2_r, x2_w, x3_r, x3_w;
 logic avm_read_r, avm_read_w, avm_write_r, avm_write_w;
-logic [15:0] rs_wdata;
-logic nxt_addr_r, mxt_addr_w;
+logic [15:0] rs_wdata_r, rs_wdata_w;
+logic nxt_addr_r, nxt_addr_w;
 
 assign avm_address = rs_addr_r;
 assign avm_read = avm_read_r;
 assign avm_write = avm_write_r;
-assign avm_writedata = {24'b0, rs_wdata[7:0]};
+assign avm_writedata = {24'b0, rs_wdata_r[7:0]};
 
 assign o_SRAM_ADDR = SRAM_addr;
-assign io_SRAM_DQ  = (state_r == S_ADD) ? add_wdata : (state_r == S_MUL) ? mul_wdata : (state_r == S_STORE) ? SRAM_wdata : 16'dz; // sram_dq as output
+assign io_SRAM_DQ  = (state_r == S_ADD && add_wen) ? add_wdata : (state_r == S_MUL && mul_wen) ? mul_wdata
+                   : (top_wen_r) ? top_wdata_r : 16'dz; // sram_dq as output
 assign SRAM_rdata  = io_SRAM_DQ; // sram_dq as input
 assign o_SRAM_WE_N = !SRAM_wen;
 assign o_SRAM_CE_N = 1'b0;
@@ -99,6 +107,7 @@ AdderUnit  #(
 	.o_wen(add_wen),
 	.o_addr(add_addr),
 	.o_wdata(add_wdata),
+    .o_varsize_x3(add_size3),
 	.o_done(add_done)
 );
 
@@ -119,6 +128,7 @@ MultUnit  #(
 	.o_wen(mul_wen),
 	.o_addr(mul_addr),
 	.o_wdata(mul_wdata),
+    .o_varsize_x3(mul_size3),
 	.o_done(mul_done)
 );
 
@@ -130,8 +140,8 @@ always_comb begin
             if (avm_waitrequest || rs_addr_r == STATUS_BASE) state_w = S_INST;
             else case (avm_readdata[7:4])
                 ADD, SUB, MUL, DIV: state_w = S_PREP1;
-                LOAD              : state_w = S_LOAD;
-                STORE             : state_w = S_PREP2;
+                LOAD              : state_w = S_SDSZ;
+                STORE             : state_w = S_GTSZ;
                 default           : state_w = S_INST;
             endcase
         end
@@ -146,68 +156,217 @@ always_comb begin
                 default : state_w = S_INST;
             endcase
         end
-        S_ADD  : state_w = add_done ? S_INST : S_ADD;
-        S_MUL  : state_w = mul_done ? S_INST : S_MUL;
-        S_LOAD : state_w = (counter_r == 2 && varsize_r <= WRDBW) ? S_INST : S_LOAD;
-        S_STORE: state_w = (counter_r == 2 && varsize_r <= WRDBW) ? S_INST : S_STORE;
+        S_ADD  : state_w = add_done ? S_STSZ1 : S_ADD;
+        S_MUL  : state_w = mul_done ? S_STSZ1 : S_MUL;
+        S_SDSZ : state_w = (counter_w == 3) ? S_LOAD : S_SDSZ;
+        S_LOAD : state_w = (counter_r == 1 && varsize_r <= WRDBW) ? S_INST  : S_LOAD;
+        S_GTSZ : state_w = (counter_w == 3) ? S_STORE : S_GTSZ;
+        S_STORE: state_w = (counter_r == 1 && varsize_r <= WRDBW) ? S_STSZ1 : S_STORE;
+        S_STSZ1: state_w = S_STSZ2;
+        S_STSZ2: state_w = S_INST;
         default: state_w = S_INST;
     endcase
 end
 
 always_comb begin
+    top_wen_w = 1'b0;
+    SRAM_addr = top_addr_r;
     case (state_r)
         S_INST : begin
             if (avm_waitrequest) begin
-                rs_addr_w = rs_addr_r;
-                avm_read_w = 1'b1;
+                rs_addr_w   = rs_addr_r;
+                avm_read_w  = 1'b1;
                 avm_write_w = 1'b0;
             end
             else if (rs_addr_r == STATUS_BASE) begin
-                rs_addr_w = avm_readdata[RX_OK_BIT] ? RX_BASE : STATUS_BASE;
-                avm_read_w = 1'b1;
+                rs_addr_w   = avm_readdata[RX_OK_BIT] ? RX_BASE : STATUS_BASE;
+                avm_read_w  = 1'b1;
                 avm_write_w = 1'b0;
             end
             else begin
-                
+                {inst_w, x3_w} = avm_readdata[7:0];
+                if (inst_w == LOAD) begin
+                    top_addr_w  = (x3_w << 2) + 20'hfffc2;
+                    top_wen_w   = 1'b0;
+                end
+                else begin
+                    x3_addr_w   = nxt_addr_r;
+                    top_addr_w  = (x3_w << 2) + 20'hfffc0;
+                    top_wen_w   = 1'b1;
+                    top_wdata_w = nxt_addr_r;
+                    varsize_x3_w = 0;
+                end
             end
-            x3_addr_w = nxt_addr_r;
-            top_addr_w = (x3_r << 2) + 20'hfffc0;
-            SRAM_wen  = 1'b0;
-            SRAM_wdata = nxt_addr_r;
         end
         S_PREP1: begin
-            top_addr_w = (x1_r << 2) + 20'hfffc0;
+            if (avm_waitrequest) begin
+                rs_addr_w   = rs_addr_r;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else if (rs_addr_r == STATUS_BASE) begin
+                rs_addr_w   = avm_readdata[RX_OK_BIT] ? RX_BASE : STATUS_BASE;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else begin
+                {x1_w, x2_w} = avm_readdata[7:0];
+                top_addr_w   = (x1_w << 2) + 20'hfffc0;
+            end
         end
         S_PREP2: begin
-            x1_addr_w = SRAM_rdata;
+            x1_addr_w  = SRAM_rdata;
             top_addr_w = top_addr_r + 2;
         end
         S_PREP3: begin
             varsize_x1_w = SRAM_rdata;
-            top_addr_w = 4*x2_r + 20'hfffc0;
+            top_addr_w   = (x2_r << 2) + 20'hfffc0;
         end
         S_PREP4: begin
-            x2_addr_w = SRAM_rdata;
+            x2_addr_w  = SRAM_rdata;
             top_addr_w = top_addr_r + 2;
         end
         S_PREP5: begin
             varsize_x2_w = SRAM_rdata;
-            varsize_w = (varsize_x1_r >= varsize_x2_r) ? varsize_x1_r : varsize_x2_r;
+            varsize_w    = (varsize_x1_r >= SRAM_rdata) ? varsize_x1_r : SRAM_rdata;
             case (inst_r)
-                ADD     : add_valid_w = 1;
-                SUB     : add_valid_w = 1;
-                MUL     : mul_valid_w = 1;
+                ADD: add_valid_w = 1;
+                SUB: add_valid_w = 1;
+                MUL: mul_valid_w = 1;
             endcase
         end
         S_ADD  : begin
             add_valid_w = 0;
             SRAM_addr = add_addr;
             SRAM_wen  = add_wen;
+            varsize_x3_w = add_done ? add_size3 : varsize_r;
         end
         S_MUL  : begin
             mul_valid_w = 0;
             SRAM_addr = mul_addr;
             SRAM_wen  = mul_wen;
+            varsize_x3_w = mul_done ? mul_size3 : varsize_r;
+        end
+        S_GTSZ : begin
+            if (avm_waitrequest) begin
+                rs_addr_w   = rs_addr_r;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else if (rs_addr_r == STATUS_BASE) begin
+                rs_addr_w   = avm_readdata[RX_OK_BIT] ? RX_BASE : STATUS_BASE;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else begin
+                varsize_x3_w = {avm_readdata[7:0], varsize_x3_r[23:8]};
+                counter_w    = counter_r + 1;
+                top_addr_w   = x3_addr_w;
+            end
+        end
+        S_STORE : begin
+            if (avm_waitrequest) begin
+                rs_addr_w   = rs_addr_r;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else if (rs_addr_r == STATUS_BASE) begin
+                rs_addr_w   = avm_readdata[RX_OK_BIT] ? RX_BASE : STATUS_BASE;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else if (counter_r == 0) begin
+                top_wdata_w = {avm_readdata[7:0], top_wdata_w[15:8]};
+                counter_w   = counter_r + 1;
+            end
+            else if (counter_r == 1) begin
+                top_wdata_w = {avm_readdata[7:0], top_wdata_w[15:8]};
+                top_wen_w   = 1'b1;
+                counter_w   = counter_r + 1;
+            end
+            else begin
+                varsize_w = varsize_r - 16;
+                top_addr_w = top_addr_r + 1;
+                counter_w = 0;
+            end
+        end
+        S_STSZ1: begin
+            top_addr_w  = (x3_r << 2) + 20'hFFFC2;
+            top_wdata_w = varsize_x3_r[15:0];
+            top_wen_w   = 1'b1;
+        end
+        S_STSZ2: begin
+            top_addr_w  = top_addr_r + 1;
+            top_wdata_w = {8'b0, varsize_x3_r[23:16]};
+            top_wen_w   = 1'b1;
+        end
+        S_SDSZ : begin
+            if (avm_waitrequest) begin
+                rs_addr_w   = rs_addr_r;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else if (rs_addr_r == STATUS_BASE) begin
+                rs_addr_w   = avm_readdata[TX_OK_BIT] ? RX_BASE : STATUS_BASE;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b1;
+            end
+            else begin
+                counter_w = counter_r + 1;
+                if (counter_r == 0) begin
+                    rs_wdata_w = SRAM_rdata[7:0];
+                    varsize_w  = SRAM_rdata;
+                    avm_write_w = 1'b1;
+                end
+                else if (counter_r == 1) begin
+                    rs_wdata_w = SRAM_rdata[15:8];
+                    top_addr_w = top_addr_r + 1;
+                    avm_write_w = 1'b1;
+                end
+                else if (counter_r == 2) begin
+                    rs_wdata_w = SRAM_rdata[7:0];
+                    varsize_w  = {SRAM_rdata[0], varsize_r[15:0]}; 
+                    top_addr_w = (x3_w << 2) + 20'hfffc0;
+                end
+                else if (counter_r == 3) begin
+                    x3_addr_w = SRAM_rdata;
+                    top_addr_w = top_addr_r + 1;
+                end
+                else begin
+                    x3_addr_w = {SRAM_rdata, x3_addr_r[15:0]};
+                    top_addr_w = x3_addr_w;
+                end
+            end
+        end
+        S_LOAD : begin
+            if (avm_waitrequest) begin
+                rs_addr_w   = rs_addr_r;
+                avm_read_w  = 1'b1;
+                avm_write_w = 1'b0;
+            end
+            else if (rs_addr_r == STATUS_BASE) begin
+                if (avm_readdata[TX_OK_BIT]) begin
+                    rs_addr_w   = STATUS_BASE;
+                    avm_read_w  = 1'b0;
+                    avm_write_w = 1'b1;
+                    rs_wdata_w  = SRAM_rdata[counter_r << 3 +: 8];
+                end
+                else begin
+                    rs_addr_w   = avm_readdata[TX_OK_BIT] ? TX_BASE : STATUS_BASE;
+                    avm_read_w  = 1'b1;
+                    avm_write_w = 1'b1;
+                end
+            end
+            if (counter_r == ) begin
+                rs_wdata_w = SRAM_rdata[15:8];
+                counter_w   = counter_r + 1;
+                avm_write_w = 1'b1;
+            end
+            else begin
+                varsize_w = varsize_r - 16;
+                top_addr_w = top_addr_r + 1;
+                counter_w = 0;
+            end
         end
     endcase
 end
